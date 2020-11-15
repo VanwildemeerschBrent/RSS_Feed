c2062bb1eb0712cfe83c4172329f32e9a797ad71
Brent Vanwildemeersch <vanwildemeersch.brent@gmail.com>
2020-11-15 12:00:40 +0100

    Fetch RSS feeds and return responses to client

--- index.js
+++ index.js
@@ -1,7 +1,28 @@
 const express = require("express");
+const Parser = require("rss-parser");
 
 const PORT = process.env.PORT || 3000;
-const app = new express();
+const app = express();
+
+const FEED_LIST = [
+  "https://sporza.be/nl.rss.xml",
+  "http://feeds.nieuwsblad.be/nieuws/snelnieuws",
+  "https://www.vrt.be/vrtnws/nl.rss.headlines.xml",
+  "https://www.wielerflits.nl/rss.php",
+  "https://www.voetbalzone.nl/rss/rss.xml",
+];
+
+app.get("/", (req, res) => {
+  let parser = new Parser();
+
+  const feedURLS = FEED_LIST.map((feed) => {
+    return parser.parseURL(feed);
+  });
+
+  Promise.all(feedURLS)
+    .then((response) => res.status(200).json(response))
+    .catch((error) => res.status(400).json(error));
+});
 
 app.listen(PORT, () => {
   console.log(`App listening on http://localhost:${PORT}`);
