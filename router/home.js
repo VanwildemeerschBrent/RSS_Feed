e68c9300e1c6f02f356d7302e2d857ac6654b38c
Brent Vanwildemeersch <vanwildemeersch.brent@gmail.com>
2020-11-15 13:48:36 +0100

    Update Layout RSS feed

--- router/home.js
+++ router/home.js
@@ -1,30 +1,29 @@
 const express = require("express");
 const Parser = require("rss-parser");
+const fs = require("fs");
+const path = require("path");
+
+const jsonFeed = fs.readFileSync(path.join(__dirname, "../feed.json"));
+const FEED_LIST = JSON.parse(jsonFeed);
 
 const Router = express.Router();
 
 module.exports = Router;
 
-const FEED_LIST = [
-  "https://sporza.be/nl.rss.xml",
-  "http://feeds.nieuwsblad.be/nieuws/snelnieuws",
-  "https://www.vrt.be/vrtnws/nl.rss.headlines.xml",
-  "https://www.wielerflits.nl/rss.php",
-  "https://www.voetbalzone.nl/rss/rss.xml",
-];
-
 Router.get("/", (req, res) => {
   let parser = new Parser();
 
   const feedURLS = FEED_LIST.map((feed) => {
-    return parser.parseURL(feed);
+    return parser.parseURL(feed.url);
   });
 
   Promise.all(feedURLS)
-    .then((response) => {
-      res.render("home", { pageTitle: "Welcome to your RSS Feed", articles: response });
-    })
+    .then((response) => res.render("home", { pageTitle: "Welcome to your RSS Feed", articles: response }))
     .catch((error) => res.status(400).json(error));
+});
+
+Router.get("/manage", (req, res) => {
+  res.render("manage");
 });
 
 Router.post("/add", (req, res) => {
