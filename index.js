0c2e4a4e801ca850088f8cb2d471e572edc5f049
Brent Vanwildemeersch <vanwildemeersch.brent@gmail.com>
2020-11-15 12:08:32 +0100

    Add routing to API

--- index.js
+++ index.js
@@ -1,28 +1,9 @@
 const express = require("express");
-const Parser = require("rss-parser");
 
 const PORT = process.env.PORT || 3000;
 const app = express();
 
-const FEED_LIST = [
-  "https://sporza.be/nl.rss.xml",
-  "http://feeds.nieuwsblad.be/nieuws/snelnieuws",
-  "https://www.vrt.be/vrtnws/nl.rss.headlines.xml",
-  "https://www.wielerflits.nl/rss.php",
-  "https://www.voetbalzone.nl/rss/rss.xml",
-];
-
-app.get("/", (req, res) => {
-  let parser = new Parser();
-
-  const feedURLS = FEED_LIST.map((feed) => {
-    return parser.parseURL(feed);
-  });
-
-  Promise.all(feedURLS)
-    .then((response) => res.status(200).json(response))
-    .catch((error) => res.status(400).json(error));
-});
+app.use("/", require("./router"));
 
 app.listen(PORT, () => {
   console.log(`App listening on http://localhost:${PORT}`);
---
+++ router.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const Parser = require("rss-parser");
+
+const Router = express.Router();
+
+module.exports = Router;
+
+const FEED_LIST = [
+  "https://sporza.be/nl.rss.xml",
+  "http://feeds.nieuwsblad.be/nieuws/snelnieuws",
+  "https://www.vrt.be/vrtnws/nl.rss.headlines.xml",
+  "https://www.wielerflits.nl/rss.php",
+  "https://www.voetbalzone.nl/rss/rss.xml",
+];
+
+Router.get("/", (req, res) => {
+  let parser = new Parser();
+
+  const feedURLS = FEED_LIST.map((feed) => {
+    return parser.parseURL(feed);
+  });
+
+  Promise.all(feedURLS)
+    .then((response) => res.status(200).json(response))
+    .catch((error) => res.status(400).json(error));
+});
+
+Router.post("/add", (req, res) => {
+  console.log("Add new RSS feed to system");
+});
